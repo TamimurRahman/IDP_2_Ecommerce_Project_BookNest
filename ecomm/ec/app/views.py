24688efd2de60c3bd091add99daca9059213d010
Tamimur <tamimr008@gmail.com>
2025-04-21 21:55:46 +0600

    Re-added ecomm/ec as regular folder after removing submodule issue

---
+++ ecomm/ec/app/views.py
@@ -0,0 +1,14 @@
+from urllib import request
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from . models import Product
+
+def home(request):
+    return render(request, 'app/home.html')
+
+class CategoryView(View):
+    def get(self, request,val):
+        product = Product.objects.filter(category=val)
+        return render(request, 'app/category.html',locals())
+    
