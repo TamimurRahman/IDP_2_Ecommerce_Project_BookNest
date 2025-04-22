215fb517e10012ed49d8eb0722ded79be609fec4
Tamimur <tamimr008@gmail.com>
2025-04-22 20:47:14 +0600

    This is the maiin product category page

--- ecomm/ec/app/views.py
+++ ecomm/ec/app/views.py
@@ -11,10 +11,19 @@ def home(request):
 class CategoryView(View):
     def get(self, request,val):
         product = Product.objects.filter(category=val)
-        title = Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
+        title = Product.objects.filter(category=val).values('title')
         return render(request, 'app/category.html',locals())
+class CategoryTitle(View):
+    def get(self, request, val):
+        # Get products matching the selected category
+        product = Product.objects.filter(title=val)
+        # Get unique titles of products in the same category
+        title = Product.objects.filter(category=product[0].category).values('title')
+        return render(request, "app/category.html", locals())
 class ProductDetail(View):
     def get(self, request,pk):
         product = Product.objects.get(pk=pk)
         return render(request, 'app/productdetail.html',locals())
     
+
+
