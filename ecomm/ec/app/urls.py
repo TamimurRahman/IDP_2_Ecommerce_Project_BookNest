16168dca8a381bbca55b4de552d54ec590f0062b
Tamimur <tamimr008@gmail.com>
2025-04-24 01:52:31 +0600

    working on login form for user valid user can login

--- ecomm/ec/app/urls.py
+++ ecomm/ec/app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm,MyPasswordResetForm
 urlpatterns = [
    path('', views.home,),
    path('about/',views.about,name='about'),
@@ -13,5 +15,8 @@ urlpatterns = [
    
    #login authentication
    path('registration/', views.CustomerRegistationView.as_view(), name='customerregistration'),
-
+   path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm), name='login'),
+   path('password-reset/',
+        auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',
+        form_class=MyPasswordResetForm),name='password_reset'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
