46a7f7c5f8dbecc6657f3fe6082709047da13ac6
Tamimur <tamimr008@gmail.com>
2025-04-24 05:15:43 +0600

    work on customer details page where customer address taken from website through login

--- ecomm/ec/app/models.py
+++ ecomm/ec/app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+STATE_CHOICES = (
+    ('Barisal', 'Barisal'),
+    ('Chattogram', 'Chattogram'),
+    ('Dhaka', 'Dhaka'),
+    ('Khulna', 'Khulna'),
+    ('Mymensingh', 'Mymensingh'),
+    ('Rajshahi', 'Rajshahi'),
+    ('Rangpur', 'Rangpur'),
+    ('Sylhet', 'Sylhet'),
+)
+
 
 CATEGORY_CHOICES = (
     ('ML', 'Milk'),
@@ -28,3 +39,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    def __str__(self):
+        return self.name
--- ecomm/ec/app/views.py
+++ ecomm/ec/app/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from . models import Product
-from .forms import CustomerRegistrationForm
+from .forms import CustomerRegistrationForm,CustomerProfileForm
 from django.contrib import messages
+from .models import Customer
+
 
 def home(request):
     return render(request, 'app/home.html')
@@ -47,4 +49,33 @@ class CustomerRegistationView(View):
             messages.warning(request, 'Invalid input data. Please try again.')
         return render(request, 'app/customerregistration.html', locals())
             
-        
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', {'form': form})
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+            
+            reg = Customer(
+                user=user,
+                name=name,
+                locality=locality,
+                city=city,
+                mobile=mobile,
+                state=state,
+                zipcode=zipcode
+            )
+            reg.save()
+            messages.success(request, "Congratulations! Profile saved successfully.")
+        else:
+            messages.warning(request, "Invalid input data.")
+        return render(request, 'app/profile.html', locals())
