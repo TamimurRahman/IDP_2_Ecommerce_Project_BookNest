2f4e8ecef5e3347fdfd35eafab5bcefc767d5736
Tamimur <tamimr008@gmail.com>
2025-04-29 20:29:40 +0600

    Restore password when user forgate password

--- ecomm/ec/app/urls.py
+++ ecomm/ec/app/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .forms import LoginForm,MyPasswordResetForm,MyPasswordChangeForm
+from .forms import LoginForm,MyPasswordResetForm,MyPasswordChangeForm,MySetPasswordForm
 urlpatterns = [
    path('', views.home,), 
    path('about/',views.about,name='about'),
@@ -18,7 +18,38 @@ urlpatterns = [
    #login authentication
    path('registration/', views.CustomerRegistationView.as_view(), name='customerregistration'),
    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm), name='login'),
-   path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm),name='password_reset'),
+   #path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm),name='password_reset'),
    path('passwordchange/',auth_views.PasswordChangeView.as_view(template_name='app/changepassword.html',form_class=MyPasswordChangeForm,success_url='/passwordchangedone/'),name='passwordchange'),
    path('passwordchangedone/',auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'),name='passwordchangedone'),
+   path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+   path('password-reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='app/password_reset.html',
+             form_class=MyPasswordResetForm
+         ), 
+         name='password_reset'
+    ),
+
+   path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='app/password_reset_done.html'
+         ), 
+         name='password_reset_done'
+    ),
+
+   path('password-reset-confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='app/password_reset_confirm.html',
+             form_class=MySetPasswordForm
+         ), 
+         name='password_reset_confirm'
+    ),
+
+   path('password-reset-complete/', 
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='app/password_reset_complete.html'
+         ), 
+         name='password_reset_complete'
+    ),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
